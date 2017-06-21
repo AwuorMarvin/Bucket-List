2f2f7ccced4afd0bc2ee237208b43296c77a3fda
Awuor Marvin <awuor.marvin5@gmail.com>
2017-06-21 02:55:53 -0700

    Add session management for logged in users

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 """Import module to render the html"""
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, session, g
 from app import app
 
 from .forms import RegisterForm, LoginForm
+from .models import User
 
 @app.route('/')
 def index():
@@ -18,8 +19,14 @@ def about():
 def login():
     """Defines the route for the login page"""
     form = LoginForm()
+    user = User()
     if form.validate_on_submit():
-        return render_template('profile.html')
+        session.pop('user', None)
+        if user.create_user()['username'] == form.username.data and user.create_user()['password'] == form.password.data:
+            flash('You were successfully logged in')
+            session['user'] = form.username.data
+            return render_template('profile.html', form=form)
+        flash("Please check your details and try again")
     return render_template('login.html', form=form)
 
 @app.route('/signup/', methods=["GET", "POST"], strict_slashes=False)
@@ -27,18 +34,39 @@ def signup():
     """Defines the route for the signup page"""
     form = RegisterForm()
     if form.validate_on_submit():
-        return render_template('profile.html')
+        return render_template('login.html')
+    else:
+        return render_template('signup.html', form=form)
     return render_template('signup.html', form=form)
 
 @app.route('/profile')
 def profile():
     """Defines the route for the profile page"""
-    return render_template('profile.html')
+    if g.user:
+        return render_template('profile.html')
+    else:
+        return render_template('home.html')
 @app.route('/add_bucketlist')
 def add_bucketlist():
     """Defines the route for the add bucketlist page"""
     return render_template('add_bucketlist.html')
 
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+
+@app.route('/getsession')
+def getsession():
+    if 'user' in session:
+        return session['user']
+    else:
+        return render_template('home.html')
+
+@app.route('/dropsession')
+def dropsession():
+    session.pop('user', None)
 #Handle errors where a template is missing
 @app.errorhandler(404)
 def not_found_error(error):
