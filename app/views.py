9d475528548ce7774b377a9d0afd24f84170aa4e
Awuor Marvin <awuor.marvin5@gmail.com>
2017-06-22 10:08:19 -0700

    Fix signup redirect to login error

--- app/views.py
+++ app/views.py
@@ -5,9 +5,8 @@ from app import app
 from .forms import RegisterForm, LoginForm
 from .models import User
 
-# new_user = User(request.form.get(LoginForm.username), request.form.get(LoginForm.password))
-# details = new_user.create_user()
-new_user = User()
+dictionary = {}
+# new_user = User()
 
 @app.route('/')
 def index():
@@ -19,27 +18,34 @@ def about():
     """Defines the route for the about page"""
     return render_template('about.html')
 
-@app.route('/login/', methods=["GET", "POST"], strict_slashes=False)
+@app.route('/login', methods=["GET", "POST"], strict_slashes=False)
 def login():
     """Defines the route for the login page"""
     form = LoginForm()
     if form.validate_on_submit():
         session.pop('user', None)
-        if new_user.login_user(form.username.data, form.password.data) is True:
+        if form.username.data in app.config['USERNAME'] and form.password.data in app.config['PASSWORD']:
             flash('You were successfully logged in')
             session['user'] = form.username.data
             return render_template('profile.html')
-        flash("Please check your details and try again")
-        render_template('login.html', form=form)
+        else:
+            flash("Please check your details and try again")
+            render_template('login.html', form=form)
     return render_template('login.html', form=form)
 
 @app.route('/signup/', methods=["GET", "POST"], strict_slashes=False)
 def signup():
     """Defines the route for the signup page"""
-    form = RegisterForm()
-    # new_user = User()
-    new_user_details = new_user.create_user(request.form.get(LoginForm.username), request.form.get(LoginForm.password))
-    if form.validate_on_submit():
+    form = RegisterForm(request.form)
+
+    if request.method == "POST" and form.validate():
+        username = form.username.data
+        password = form.password.data
+
+        app.config['USERNAME'].append(username)
+        app.config['PASSWORD'].append(password)
+
+        flash('Signed up as {}, please log in to continue'.format(username))
         return redirect(url_for('login'))
     else:
         return render_template('signup.html', form=form)
@@ -47,14 +53,25 @@ def signup():
 @app.route('/profile')
 def profile():
     """Defines the route for the profile page"""
+    # profile= {}
     if g.user:
-        return render_template('profile.html')
+        return render_template('profile.html', data=dictionary)
     else:
         return render_template('home.html')
-@app.route('/add_bucketlist')
+@app.route('/add_bucketlist', methods=['GET', 'POST'])
 def add_bucketlist():
     """Defines the route for the add bucketlist page"""
-    return render_template('add_bucketlist.html')
+    bucketlist = {}
+    if request.method == "POST":
+        title = request.form['inputTitle']
+        description = request.form['inputDescription']
+
+        bucketlist['title'] = title
+        bucketlist['body'] = description
+        
+        return render_template('profile.html', data=bucketlist)
+    else:
+        return render_template('add_bucketlist.html', data=bucketlist)
 
 @app.before_request
 def before_request():
@@ -62,16 +79,16 @@ def before_request():
     if 'user' in session:
         g.user = session['user']
 
-@app.route('/getsession')
-def getsession():
-    if 'user' in session:
-        return session['user']
-    else:
-        return render_template('home.html')
+# @app.route('/getsession')
+# def getsession():
+#     if 'user' in session:
+#         return session['user']
+#     else:
+#         return render_template('home.html')
 
-@app.route('/dropsession')
-def dropsession():
-    session.pop('user', None)
+# @app.route('/dropsession')
+# def dropsession():
+#     session.pop('user', None)
 
 @app.route('/logout')
 def logout():
